561da44fc3ac3eba0409408e863e2c73cb1c4afe
sherenakhansaz <sherenakhansa@gmail.com>
2024-11-08 08:56:13 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+// String
+let greeting = "Halo, Semuanya!";
+let name = "Windah Basudara";
+console.log(greeting + " Nama saya adalah "); 
+
+// Boolean
+let a = 10;
+let b = 10;
+let c = 15;
+let d = "10";
+
+console.log(a == b); 
+console.log(a == c); 
+console.log(a == d);
+
+// Numbers
+let age = 30;
+console.log("Usia:", age + 20); 
+
+// Array
+let flowers = ["Rose", "Tulip", "Lily"];
+alert(flowers[0]); 
+alert(flowers[1]); 
+alert(flowers[2]); 
+flowers[2] = "Sunflower"; 
+alert(flowers.length); 
+alert(flowers); 
+
+// Object
+const book = {
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: "J.K. Rowling",
+    year: 1997};
+console.log(book.title);  
+console.log(book.author); 
+console.log(book.year);  
+
+// Console Log
+const model = {
+    name: "Gigi Hadid",
+    age: 28,
+    nationality: "American"};
+    console.log(model);
+
+// Logical Operator
+let x = 8;
+let y = 20;
+console.log(x < 10 && y > 15); 
+console.log(x > 10 && y > 15); 
+console.log(x < 10 || y < 15); 
+console.log(x > 10 || y < 15); 
+console.log(!(x > 5)); 
+console.log(!(x < 10 && y > 15));
+
+// Equality Comparison
+const num1 = 5;
+const num2 = '5';
+console.log(num1 == num2);  
+console.log(num1 === num2); 
+
